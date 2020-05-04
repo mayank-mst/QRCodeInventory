39c0d72758439ca49debf02e238bce189b2ed6a1
Mayank Singh <mayank.mst18@gamil.com>
2020-05-04 09:45:19 +0530

    Initial commit

---
+++ app/src/main/java/com/cdac/qrcodescanner/MainActivity.java
@@ -0,0 +1,24 @@
+package com.cdac.qrcodescanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button bscan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bscan = (Button) findViewById(R.id.buttonScanBarcode);
+        bscan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in= new Intent(MainActivity.this,ScannerBarcodeActivity.class);
+                startActivity(in);
+            }
+        });
+    }
+}
